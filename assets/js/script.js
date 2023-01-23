99ec98054c7f58cc1ac050fb36f91eb31359bbe4
DessySI <divanova.docs.manager@gmail.com>
2023-01-23 22:57:55 +0000

    button revised

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 
+var hoursArr = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
+var storeWorkingHours = function() {
+    localStorage.setItem("hoursArr", JSON.stringify(hoursArr));
+  }
 
+  $(document).ready(function() {
 //displays the current day at the top of the calendar when a user opens the planner.
 var now = moment().format("dddd, MMMM DDDo");
 $("#currentDay").text(now);
@@ -12,18 +17,15 @@ var newDiv=$('<div>')
 for (var i=0; i<9; i++){
     newDiv.clone().appendTo('#main').addClass('row');
 };
-
 //append coluns to row
-newDiv.clone().appendTo('.row').addClass('col-1 hours ');
+newDiv.clone().appendTo('.row').addClass('col-1 hours');
 newDiv.clone().appendTo('.row').addClass('col-10 text-area');
 newDiv.clone().appendTo('.row').addClass('col-1 save-button');
 
 //add hour to every first column
-var hoursArr = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-
-for (var i = 0; i < hoursArr.length; i++) {
-        $('.hours').eq(i).append("<p>" + hoursArr[i] + "</p>");
-    console.log(hoursArr);
+var i=0;
+for (i = 0; i < hoursArr.length; i++) {
+        $('.hours').eq(i).append(hoursArr[i]);
 }
 
 //add textarea to every second column
@@ -32,10 +34,15 @@ addTextArea.append("<textarea>").addClass('textarea');
 
 //add save button to every third column
 var addSaveButton = $('.save-button');
-addSaveButton.append("<button>").addClass('saveBtn');
+addSaveButton.append("<i>").addClass('fas fa-save saveBtn');
 
-//new variable to get the hour
-var hourNow = moment().format("dddd, MMMM DDDo");
+//variable to get the hour
+var hourNow = moment().hour();
+console.log(hourNow);
+});
+
+
+
 
 
 
