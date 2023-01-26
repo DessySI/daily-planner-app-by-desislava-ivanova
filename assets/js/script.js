13a609a53d9e030e0a400535efe513f5775f769f
DessySI <divanova.docs.manager@gmail.com>
2023-01-26 11:40:15 +0000

    col classes updated in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
+$(document).ready(function() {
+
 var hoursArr = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
 var now = moment().format("dddd, MMMM DDDo");
 
 $("#currentDay").text(now);
 //function that executes the code upon start/restart
- $(document).ready(function() {
+
 //displays the current day at the top of the calendar when a user opens the planner.
 //build grid
 var newDiv=$('<div>');
@@ -17,11 +19,11 @@ for (var i=0; i<9; i++){
 var newButton=$('<button>');
 var newTextArea=$('<textarea>');
 
-newDiv.clone().appendTo('.row').addClass('col-1 hour');
+newDiv.clone().appendTo('.row').addClass('col-2 col-lg-1 hour');
 //add textarea to every second column
-newTextArea.clone().appendTo('.row').addClass('col-10 text-area');
+newTextArea.clone().appendTo('.row').addClass('col-8 col-lg-10 text-area');
 //add save button to every third column
-newButton.clone().appendTo('.row').addClass('col-1 fas fa-save saveBtn');
+newButton.clone().appendTo('.row').addClass('col-2 col-lg-1 fas fa-save saveBtn');
 
 //add hour and id to hour and id to textarea
 
@@ -51,8 +53,6 @@ $('.time-block').each(function () {
     $(this).addClass('present');
   }if(hourIndex > hourNow){
     $(this).addClass('future');
-  }if(hourNow.change){
-    $(document).ready.function();
   }
   console.log(hourNow, hourIndex);
 });
